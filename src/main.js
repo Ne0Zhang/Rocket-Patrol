61429bf5e6117563cfd558d0520912c8a93664d7
Neo Zhang <kzhang74@ucsc.edu>
2021-04-09 18:05:29 -0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config); 
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+let starSpeed = 4;  // 4 pixel per fram
+
+let keyF, keyR, keyLEFT, keyRIGHT;
